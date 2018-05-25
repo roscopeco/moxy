264ec0f1a7fa7c3e43ddf7e0523bdf9539dbb5f4
Ross Bamford <roscopeco@gmail.com>
2018-05-25 11:07:31 +0100

    Package-private constructor

--- src/main/java/com/roscopeco/moxy/matchers/AnyMatcher.java
+++ src/main/java/com/roscopeco/moxy/matchers/AnyMatcher.java
@@ -1,6 +1,8 @@
 package com.roscopeco.moxy.matchers;
 
 public class AnyMatcher<T> implements MoxyMatcher<T> {
+  AnyMatcher() { }
+  
   @Override
   public boolean matches(T arg) {
     return true;

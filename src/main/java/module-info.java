bd994e75605dad04881933da687a9f5e1ba3b06c
Ross Bamford <roscopeco@gmail.com>
2018-05-29 17:38:56 +0100

    First cut Java9 modularisation

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.roscopeco.moxy {
+  requires jdk.unsupported;
+  requires java.logging;
+  requires org.objectweb.asm;
+  requires org.objectweb.asm.tree;
+  requires org.objectweb.asm.util;
+  requires org.opentest4j;
+}

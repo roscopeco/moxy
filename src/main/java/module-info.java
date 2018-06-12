179b5b18fd8fa67b8a97a75d79566f1d1baad114
Ross Bamford <roscopeco@gmail.com>
2018-06-12 23:52:34 +0100

    Add commons-lang3 to required modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,11 @@
 module com.roscopeco.moxy {
   exports com.roscopeco.moxy;
-  
+
   requires jdk.unsupported;
   requires java.logging;
   requires org.objectweb.asm;
   requires org.objectweb.asm.tree;
   requires org.objectweb.asm.util;
   requires org.opentest4j;
+  requires org.apache.commons.lang3;
 }
